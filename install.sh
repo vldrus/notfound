e21d7230b1d5a65d531b116d843e7c7e590fd72d
Vlad <vldrus@gmail.com>
2025-11-24 07:35:56 +0300

    notfound

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 echo -e '\n🟩 INSTALLING NOTFOUND...\n'
 
-wget -O notfound.tar.gz https://github.com/vldrus/notfound/releases/download/v1.0.0/notfound-linux-amd64.tar.gz
-echo '82b067fb9c62288b21a81ce7b5d4fedee04a2fca *notfound.tar.gz' | sha1sum -c
+wget -O notfound.tar.gz https://github.com/vldrus/notfound/releases/download/v1.0.1/notfound-linux-amd64.tar.gz
+echo '2365b31f0b0ccfef70baf7f36f3309b5033d4a9a *notfound.tar.gz' | sha1sum -c
 tar -x -C /opt -f notfound.tar.gz
 rm notfound.tar.gz
 
