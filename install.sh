362f1d075cb2c8907247fb6b4aa97b72636fcf8d
Vladislav A <vldrus@gmail.com>
2025-12-03 07:38:55 +0300

    notfound

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ mkdir -p /usr/local/lib/systemd/system
 wget -O notfound.tar.gz https://github.com/vldrus/notfound/releases/download/v1.0.2/notfound-linux-amd64.tar.gz
 echo '0212ba3996c797de77e5bb255ec533ebf28327f2011abda6d30b68e11a346677 *notfound.tar.gz' | sha256sum -c
 
-tar -x -C /usr/local/bin                -f notfound.tar.gz --strip-components=1 notfound/notfound
-tar -x -C /usr/local/lib/systemd/system -f notfound.tar.gz --strip-components=1 notfound/notfound.service
+tar -x --strip-components=1 -C /usr/local/bin                -f notfound.tar.gz notfound/notfound
+tar -x --strip-components=1 -C /usr/local/lib/systemd/system -f notfound.tar.gz notfound/notfound.service
 
 rm notfound.tar.gz
 
