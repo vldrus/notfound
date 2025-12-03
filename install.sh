a414c40bc40670f4ac2572bb938f414ae2d6bde4
Vladislav A <vldrus@gmail.com>
2025-12-03 07:35:47 +0300

    notfound

--- install.sh
+++ install.sh
@@ -2,14 +2,18 @@
 
 echo -e '\n🟩 Installing NotFound...\n'
 
+mkdir -p /usr/local/bin
+mkdir -p /usr/local/lib/systemd/system
+
 wget -O notfound.tar.gz https://github.com/vldrus/notfound/releases/download/v1.0.2/notfound-linux-amd64.tar.gz
 echo '0212ba3996c797de77e5bb255ec533ebf28327f2011abda6d30b68e11a346677 *notfound.tar.gz' | sha256sum -c
-tar -x -C /opt -f notfound.tar.gz
+
+tar -x -C /usr/local/bin                -f notfound.tar.gz --strip-components=1 notfound/notfound
+tar -x -C /usr/local/lib/systemd/system -f notfound.tar.gz --strip-components=1 notfound/notfound.service
+
 rm notfound.tar.gz
 
-cp /opt/notfound/notfound.service /etc/systemd/system/notfound.service
-
-systemctl enable notfound
-systemctl start notfound
+systemctl enable notfound.service
+systemctl start  notfound.service
 
 echo -e '\n🟩 Installing NotFound DONE\n'
